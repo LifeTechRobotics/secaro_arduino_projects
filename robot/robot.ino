b3b46e68701b47b861206779a8afc350991239b7
Norah <rinrei_i@ltrobo.co.jp>
2025-04-21 18:01:45 +0900

    update servo robot

--- robot/robot.ino
+++ robot/robot.ino
@@ -6,19 +6,26 @@
 #define DUTY_MID 5000
 #define DUTY_HIGH 8500
 #define DUTY_STEP 1000
-#define TIMER_WIDTH 16
-#define CHANNEL_1 1
-#define CHANNEL_2 2
 
 #include "esp32-hal-ledc.h"
 #include "M5Atom.h"
 
+const int ledPin1 = 19;       // Servo1 PIN 19 を使用
+const int ledPin2 = 22;       // Servo2 PIN 22 を使用
+const int pwmChannel1 = 1;    // チャンネル 1 を使う
+const int pwmChannel2 = 2;    // チャンネル 2 を使う
+const int freq = 50;         // PWM 周波数
+const int resolution = 16;   // 8ビットの分解能（0～255）
+
 void setup() {
-    M5.begin(true, false, true);  // Initialize serial port, LED
-    ledcSetup(CHANNEL_1, 50, TIMER_WIDTH);
-    ledcSetup(CHANNEL_2, 50, TIMER_WIDTH);
-    ledcAttachPin(19, CHANNEL_1);
-    ledcAttachPin(22, CHANNEL_2);
+    // M5 の初期化
+    M5.begin(true, false, true);
+    // PWM チャンネルの初期化    
+    ledcSetup(pwmChannel1, freq, resolution);
+    ledcSetup(pwmChannel2, freq, resolution);
+    // チャンネルとピンの紐づけ
+    ledcAttachPin(ledPin1, pwmChannel1);
+    ledcAttachPin(ledPin2, pwmChannel2);
 }
 
 bool run = true;
@@ -26,28 +33,28 @@ bool run = true;
 void loop() {
   if(run == true) {
     //前進
-    ledcWrite(CHANNEL_1, DUTY_MID - DUTY_STEP);
-    ledcWrite(CHANNEL_2, DUTY_MID + DUTY_STEP);
-    delay(2000);
-    
-    //後退
-    ledcWrite(CHANNEL_1, DUTY_MID + DUTY_STEP);
-    ledcWrite(CHANNEL_2, DUTY_MID - DUTY_STEP);
-    delay(2000);
-    
-    //正回転
-    ledcWrite(CHANNEL_1, DUTY_MID + DUTY_STEP);
-    ledcWrite(CHANNEL_2, DUTY_MID + DUTY_STEP);
+    ledcWrite(pwmChannel1, DUTY_MID + DUTY_STEP);
+    ledcWrite(pwmChannel2, DUTY_MID - DUTY_STEP);
     delay(2000);
 
-    //逆回転
-    ledcWrite(CHANNEL_1, DUTY_MID - DUTY_STEP);
-    ledcWrite(CHANNEL_2, DUTY_MID - DUTY_STEP);
+    //後退
+    ledcWrite(pwmChannel1, DUTY_MID - DUTY_STEP);
+    ledcWrite(pwmChannel2, DUTY_MID + DUTY_STEP);
+    delay(2000);
+    
+    //左旋回
+    ledcWrite(pwmChannel1, DUTY_MID - DUTY_STEP);
+    ledcWrite(pwmChannel2, DUTY_MID - DUTY_STEP);
+    delay(2000);
+
+    //右旋回
+    ledcWrite(pwmChannel1, DUTY_MID + DUTY_STEP);
+    ledcWrite(pwmChannel2, DUTY_MID + DUTY_STEP);
     delay(2000);
 
     // 停止
-    ledcWrite(CHANNEL_1, 0);
-    ledcWrite(CHANNEL_2, 0);
+    ledcWrite(pwmChannel1, 0);
+    ledcWrite(pwmChannel2, 0);
 
     run = false;
   }
--- servo/servo.ino
+++ servo/servo.ino
@@ -1,18 +1,23 @@
 //Control rotation angle or speed and direction by sending pulse 
-//Servo PIN 19
 
 #define DUTY_LOW 1500
 #define DUTY_MID 5000
 #define DUTY_HIGH 8500
-#define TIMER_WIDTH 16
-#define CHANNEL_1 1
 #include "esp32-hal-ledc.h"
 #include "M5Atom.h"
 
+const int ledPin = 19;       // Servo PIN 19 を使用
+const int pwmChannel = 1;    // チャンネル 1 を使う
+const int freq = 50;         // PWM 周波数
+const int resolution = 16;   // 8ビットの分解能（0～255）
+
 void setup() {
-    M5.begin(true, false, true);  // Initialize serial port, LED
-    ledcSetup(CHANNEL_1, 50, TIMER_WIDTH);
-    ledcAttachPin(19, CHANNEL_1);
+    // M5 の初期化
+    M5.begin(true, false, true);
+    // PWM チャンネルの初期化
+    ledcSetup(pwmChannel, freq, resolution);
+    // チャンネルとピンの紐づけ
+    ledcAttachPin(ledPin, pwmChannel);
 }
 
 bool run = true;
@@ -20,23 +25,23 @@ bool run = true;
 void loop() {
   if(run == true) {
     //正回転
-    for(int i = DUTY_MID; i > DUTY_LOW; i = i - 100){  
-      ledcWrite(CHANNEL_1, i);
-      delay(50);
-    }
-    
-    // 停止
-    ledcWrite(CHANNEL_1, 0);
-    delay(1000);
-    
-    //逆回転
     for(int i = DUTY_MID; i < DUTY_HIGH; i = i + 100){  
-      ledcWrite(CHANNEL_1, i);
+      ledcWrite(pwmChannel, i);
       delay(50);
     }
 
     // 停止
-    ledcWrite(CHANNEL_1, 0);
+    ledcWrite(pwmChannel, 0);
+    delay(1000);
+    
+    //逆回転
+    for(int i = DUTY_MID; i > DUTY_LOW; i = i - 100){  
+      ledcWrite(pwmChannel, i);
+      delay(50);
+    }
+    
+    // 停止
+    ledcWrite(pwmChannel, 0);
 
     run = false;
   }
